76e8461ef3817fe2ae7e3a35de3b6b9a0d000ecd
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-01-25 11:55:05 -0800

    3 pairs of fetchcalls complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
+import {
+  fetchCharacters,
+  fetchCharactersThen,
+  fetchMachines,
+  fetchMachinesThen,
+  fetchRickMorty,
+  fetchRickMortyThen,
+} from './services/promise-me';
+import { useEffect } from 'react';
+
 export default function App() {
+  useEffect(() => {
+    fetchMachinesThen();
+    fetchMachines();
+    fetchCharacters();
+    fetchCharactersThen();
+    fetchRickMorty();
+    fetchRickMortyThen();
+  });
   return <h1>Hello World</h1>;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,35 +1,53 @@
-/**
- * TODO:
- */
+// #1 pinball
 
-/**
- * TODO:
- */
+export const fetchMachines = async function () {
+  const resp = await fetch(`https://ponball.herokuapp.com/api/v1/machines`);
+  const machineData = await resp.json();
+  console.log('machineData', machineData);
+  return machineData;
+};
 
-/**
- * TODO:
- */
+export const fetchMachinesThen = () => {
+  const resp = fetch(`https://ponball.herokuapp.com/api/v1/machines`)
+    .then((data) => data.json())
+    .then((machineThenData) => {
+      console.log('machineThenData', machineThenData);
+    });
+  return resp;
+};
 
-/**
- * TODO:
- */
+// #2
 
-/**
- * TODO:
- */
+export const fetchCharacters = async () => {
+  const resp = await fetch(`https://www.breakingbadapi.com/api/characters`);
+  const charactarData = await resp.json();
+  console.log('charactarData', charactarData);
+  return charactarData;
+};
 
-/**
- * TODO:
- */
+export const fetchCharactersThen = () => {
+  const resp = fetch(`https://www.breakingbadapi.com/api/characters`)
+    .then((data) => data.json())
+    .then((charactersThenData) => {
+      console.log('charactersThenData', charactersThenData);
+    });
+  return resp;
+};
 
-/**
- * TODO:
- */
+// #3
 
-/**
- * TODO:
- */
+export const fetchRickMorty = async () => {
+  const resp = fetch(`https://rickandmortyapi.com/api/episode`);
+  const episodeData = await (await resp).json();
+  console.log('episodeData.results', episodeData);
+  return episodeData;
+};
 
-/**
- * TODO:
- */
+export const fetchRickMortyThen = () => {
+  const resp = fetch(`https://rickandmortyapi.com/api/episode`)
+    .then((data) => data.json())
+    .then((episodeThenData) => {
+      console.log('episodeThenData', episodeThenData);
+    });
+  return resp;
+};
